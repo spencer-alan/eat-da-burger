b0543f0db4b8bb610ed8730296c23b2d4e468f8b
b1acksmyth <smith.spence13@gmail.com>
2020-09-20 14:05:47 -0700

    added env package and finished connection file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,17 @@
-const connection = {
+const mysql = require("mysql");
+require("dotenv").config();
 
-};
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.SQL_PWD,
+  database: "burgers"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to Database at " + connection.threadId + "\n");
+});
 
 module.exports = connection;
