5cc15ccee5ce6afddf067c563deed67275fe09c9
b1acksmyth <smith.spence13@gmail.com>
2020-09-20 15:35:11 -0700

    working on selectAll

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const burger = require("../models/burger");
+const burger = require("../models/burger.js");
 const router = express.Router();
 
-
-
-
+router.get("/", function(req, res) {
+  burger.selectAll(function(result) {
+    // if (err) throw err;
+    console.log("Results: ", result);
+    res.render("index", { burgers: result }); 
+  });
+});
 
 module.exports = router;
