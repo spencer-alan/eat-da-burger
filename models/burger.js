b71f90cabbbee34833520ea118d4ddc3570bb55e
b1acksmyth <smith.spence13@gmail.com>
2020-09-20 15:32:31 -0700

    select all function added/ needs work

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,12 @@
 const orm = require("../config/orm");
+
+const burger = {
+  selectAll: function(cb) {
+    orm.selectAll(function(result) {
+      console.log(result);
+      cb(result);
+    });
+  }
+};
+
+module.exports = burger;
